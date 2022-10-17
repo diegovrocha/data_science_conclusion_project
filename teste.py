1efefcfd6f889055feeba72be4ebc8489ec97ec8
Diêgo Vieira <diego.vieira@crednosso.com.br>
2022-10-17 08:45:56 -0300

    melhorias

---
+++ teste.py
@@ -0,0 +1,9 @@
+'national','origin','race','religion','marital','status','diversity','company',
+'gender','sexual','color','national','milions','people','dental','vision',
+'race','color','wed','love','religion','sex','discriminate','basis',
+'orientation','gender','medical','dental','basis','race','origin','gender',
+'equal','opportunit','equal','employment','sexual','orientation','san','francisco'
+'accommodations','eg','dont','entire','could', 'life','passionate','hoppers',
+'together','many','know','future','variety','quora','insurance','needs',
+'identity','health','want', 'hopper','well','millions','please','everyone',
+'great', 'share', 'believe', 'join','like','make','youll','us','new','help'
